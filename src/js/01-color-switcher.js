980266e12ce0c7d1574b09f716a7d6a5880acdbe
Andrij Csuhran <4usika@gmail.com>
2023-06-02 11:50:40 +0200

    Update 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId;
+
+startBtn.addEventListener('click', startColorChange);
+
+function startColorChange() {
+    startBtn.disabled = true;
+    intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function changeBackgroundColor() {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+stopBtn.addEventListener('click', stopColorChange);
+
+function stopColorChange() {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+}
