f55cb59ed2048ce8d2f80a4ccbe7816e114eca0c
fairycreator <122693095+fairycreator@users.noreply.github.com>
2023-06-09 10:19:16 +0000

    Deploying to gh-pages from @ fairycreator/goit-js-hw-09@e8f536318b1e97c5aef62c6555658a97e89a805d 🚀

--- 01-color-switcher.be179c79.js
+++
@@ -1,2 +0,0 @@
-!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");function n(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"));document.body.style.backgroundColor=t}e.addEventListener("click",(function(){e.disabled=!0,t=setInterval(n,1e3)})),a.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.be179c79.js.map
---
+++ 01-color-switcher.f21dd919.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");function d(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"));document.body.style.backgroundColor=t}e.addEventListener("click",(function(){e.disabled=!0,a.disabled=!1,t=setInterval(d,1e3)})),a.addEventListener("click",(function(){clearInterval(t),a.disabled=!0,e.disabled=!1})),a.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.f21dd919.js.map
